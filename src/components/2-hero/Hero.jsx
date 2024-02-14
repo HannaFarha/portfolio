c1f9c7119964145a8c863c204b65c4131c849ab0
HannaFarha <johnnyfarha1@gmail.com>
2024-02-14 12:41:03 +0100

    hero done

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -1,7 +1,62 @@
-import React  from "react";
-import "./hero.css"
+import Lottie from "lottie-react";
+import "./hero.css";
+
+import { useRef } from "react";
+
+
 const Hero = () => {
-    return ( <div>Hero</div> );
-}
- 
+  const lottieRef = useRef();
+
+  return (
+    <section className="hero flex">
+      <div className="left-section  ">
+        <div className="parent-avatar flex">
+          <img
+            initial={{ transform: "scale(0)" }}
+            animate={{ transform: "scale(1.1)" }}
+            transition={{ damping: 6, type: "spring", stiffness: 100 }}
+            src="./me.png"
+            className="avatar"
+            alt=""
+          />
+          <div className="icon-verified"></div>
+        </div>
+
+        <h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="title"
+        >
+          Software designer, founder, and amateur astronaut.
+        </h1>
+
+        <p className="sub-title">
+          I’m Hanna Farha, 
+        </p>
+
+        <div className="all-icons flex">
+          <div className="icon icon-twitter"></div>
+          <div className="icon icon-instagram"></div>
+          <div className="icon icon-github"></div>
+          <div className="icon icon-linkedin"></div>
+        </div>
+      </div>
+
+      <div className="right-section animation ">
+        <Lottie
+          lottieRef={lottieRef}
+          className=""
+          onLoadedImages={() => {
+            // @ts-ignore
+            // https://lottiereact.com/
+            lottieRef.current.setSpeed(0.5);
+          }}
+         
+        />
+      </div>
+    </section>
+  );
+};
+
 export default Hero;
