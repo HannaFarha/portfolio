f9e73387411847169b3e1a18cf9923d4b1b51682
HannaFarha <johnnyfarha1@gmail.com>
2024-02-15 12:54:12 +0100

    arrow up button

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,10 @@ function App() {
     <Contact />
     <div className='divider' />
     <Footer />
+
+    <a style={{  transition: "1s" }} href="#up">
+        <button className="icon-keyboard_arrow_up scroll2Top"></button>
+      </a>
     </div>
   )
 }
