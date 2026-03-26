e812a4908ef18c45d8a1840901971f864926d2d7
HannaFarha <johnnyfarha1@gmail.com>
2026-03-26 14:54:33 +0100

    Best

--- src/components/5-footer/Footer.jsx
+++ src/components/5-footer/Footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
     <footer className="flex">
       <ul className="flex">
         <li>
-          <a href="about">About</a>
+          <a href="#about">About</a>
         </li>
         <li>
-          <a href="projects">Projects</a>
+          <a href="#projects">Projects</a>
         </li>
         <li>
-          <a href="contact">Contact</a>
+          <a href="#contact">Contact</a>
         </li>
         <li>
           <a href="">Datenschutz</a>
